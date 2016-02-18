b936c629bf268ac4ef040d83e9323f851f95f824
Andy Chong Chin Shin <andychong2@gmail.com>
2016-02-18 10:21:05 +0800

    contributing guide

--- crawler/news_crawler.py
+++ crawler/news_crawler.py
@@ -5,6 +5,7 @@ from tweeter_key import *
 
 
 class Model:
+
   def __init__(self, text, author, datetime):
     self.text = text
     self.author = author
@@ -12,9 +13,10 @@ class Model:
 
 
 class ModelEncoder(json.JSONEncoder):
+
   def default(self, obj):
     if isinstance(obj, Model):
-        return {"text": obj.text, "author": obj.author, "datetime": obj.datetime}
+      return {"text": obj.text, "author": obj.author, "datetime": obj.datetime}
     # Let the base class default method raise the TypeError
     return json.JSONEncoder.default(self, obj)
 
@@ -23,7 +25,7 @@ epoch = datetime.datetime.utcfromtimestamp(0)
 
 
 def unix_time_millis(dt):
-    return (dt - epoch).total_seconds() * 1000.0
+  return (dt - epoch).total_seconds() * 1000.0
 
 
 def crawl(name, total_page):
@@ -35,11 +37,11 @@ def crawl(name, total_page):
   result = []
   api = tweepy.API(auth)
 
-  for i in range(1,total_page + 1):
+  for i in range(1, total_page + 1):
     print 'crawling page', i
 
     tweets = api.user_timeline(name, count=200, page=i)
-    
+
     for tweet in tweets:
       text = tweet.text
       author = tweet.author.name
