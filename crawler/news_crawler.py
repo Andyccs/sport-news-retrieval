590339adff42ad0b149092c529e97356df34c671
Andy Chong Chin Shin <andychong2@gmail.com>
2016-02-18 10:17:19 +0800

    crawler with readme done

--- crawler/news_crawler.py
+++ crawler/news_crawler.py
@@ -1,16 +1,62 @@
+import datetime
+import json
 import tweepy
 from tweeter_key import *
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+class Model:
+  def __init__(self, text, author, datetime):
+    self.text = text
+    self.author = author
+    self.datetime = datetime
 
-for i in range(1,51):
-  tweets = api.user_timeline('espn', count=200, page=i)
-  print 'page', i, 'count', len(tweets)
-  # for tweet in tweets:
-  #   print tweet.text
-  #   print '========================='
 
-# espn has 200 data in the first 16 page, and 1 data in 17 page
+class ModelEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, Model):
+        return {"text": obj.text, "author": obj.author, "datetime": obj.datetime}
+    # Let the base class default method raise the TypeError
+    return json.JSONEncoder.default(self, obj)
+
+
+epoch = datetime.datetime.utcfromtimestamp(0)
+
+
+def unix_time_millis(dt):
+    return (dt - epoch).total_seconds() * 1000.0
+
+
+def crawl(name, total_page):
+  print 'crawling', name, 'tweeter timeline'
+
+  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_token, access_token_secret)
+
+  result = []
+  api = tweepy.API(auth)
+
+  for i in range(1,total_page + 1):
+    print 'crawling page', i
+
+    tweets = api.user_timeline(name, count=200, page=i)
+    
+    for tweet in tweets:
+      text = tweet.text
+      author = tweet.author.name
+      created_at = tweet.created_at
+
+      data = Model(text, author, unix_time_millis(created_at))
+      result.append(data)
+
+    print 'crawled', len(tweets), 'tweets'
+
+  result_json = json.dumps(result, cls=ModelEncoder)
+
+  f = open(name + '_data.json', 'w')
+  f.write(result_json)
+  f.close()
+
+
+if __name__ == "__main__":
+  crawl('espn', 17)
+  crawl('TheNBACentral', 17)
