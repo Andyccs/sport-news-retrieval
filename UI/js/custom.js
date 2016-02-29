09b0f357a4ca27471b4d72fc52429e585662f80f
Andyccs <y4andy@gmail.com>
2016-02-29 22:01:29 +0800

    linting javascript using eslint

--- UI/js/custom.js
+++ UI/js/custom.js
@@ -1,20 +1,22 @@
-$("#crawl").click(function(){
-	//Here initialize a recrawling request to backend
-	//Upon finished, generate an alert window
-	alert("Finish Recrawling");
-})
+$('#crawl').click(function() {
+  // Here initialize a recrawling request to backend
+  // Upon finished, generate an alert window
+  alert('Finish Recrawling');
+});
 
 var app = angular.module('myApp', []);
-app.controller('newsCtrl', function($scope, $http){
-		
-	$scope.keywords = "Enter keywords here";
-	$("#search").click(function(){
-		//Later, need to modify this http request so that it makes query to backend solr using keywords		
-		var keywords = $scope.keywords;
-		$http.get("fake_news.json").success(
-			function(response) {
-	          		     $scope.news = response.news;
-	    		});
-	})
-		
- });
+
+app.controller('newsCtrl', function($scope, $http) {
+
+  $scope.keywords = 'Enter keywords here';
+  $('#search').click(function() {
+    // TODO(RUAN0007): need to modify this http request so that it makes query to backend solr
+    // using keywords
+    var keywords = $scope.keywords;
+
+    $http.get('fake_news.json').success(
+      function(response) {
+        $scope.news = response.news;
+      });
+  });
+});
