0cd4a6e9d0b2ee3511c38f7e533948482964b98b
Andy Chong Chin Shin <andychong2@gmail.com>
2016-03-03 15:26:47 +0800

    remove binary temp file, and refine code style

--- UI/js/custom.js
+++ UI/js/custom.js
@@ -7,24 +7,18 @@ $('#crawl').click(function() {
 var app = angular.module('myApp', []);
 
 app.controller('newsCtrl', function($scope, $http) {
-	
-	
+
   $scope.comment = "Popular searches: Warriors, Curry for Three";
   $('#search').click(function() {
-    // TODO(RUAN0007): need to modify this http request so that it makes query to backend solr
-    // using keywords
     var keywords = $scope.keywords;
     var url = "http://localhost:8983/solr/sport/select?json.wrf=JSON_CALLBACK&q=" + keywords + "&wt=json&indent=true";
-    
-    $http .jsonp(url) .success(function(data) {
 
-	    $scope.news = data.response.docs ;
-      
-            var queryTime = data.responseHeader.QTime;
-    	   $scope.comment = "The query takes " + queryTime + " milliseconds. ";
-   });
+    $http.jsonp(url).success(function(data) {
 
+      $scope.news = data.response.docs ;
 
-      
+      var queryTime = data.responseHeader.QTime;
+      $scope.comment = "The query takes " + queryTime + " milliseconds. ";
+    });
   });
 });
