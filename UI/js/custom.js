065ac6b91372dc063771f2c6b294240cb167e6ca
Ruan Pingcheng <ruanpingcheng@gmail.com>
2016-03-03 11:00:33 +0800

    Make angularjs query local solr

--- UI/js/custom.js
+++ UI/js/custom.js
@@ -9,19 +9,22 @@ var app = angular.module('myApp', []);
 app.controller('newsCtrl', function($scope, $http) {
 	
 	
-	$scope.comment = "Popular searches: Warriors, Curry for Three";
-  $scope.keywords = 'Enter keywords here';
+  $scope.comment = "Popular searches: Warriors, Curry for Three";
   $('#search').click(function() {
     // TODO(RUAN0007): need to modify this http request so that it makes query to backend solr
     // using keywords
     var keywords = $scope.keywords;
-    $http.get('fake_news.json').success(
-      function(response) {
-        $scope.news = response.news;
-      });
+    var url = "http://localhost:8983/solr/sport/select?json.wrf=JSON_CALLBACK&q=" + keywords + "&wt=json&indent=true";
+    
+    $http .jsonp(url) .success(function(data) {
+
+	    $scope.news = data.response.docs ;
       
-	var queryTime = 4.2;
-	$scope.comment = "The query takes " + queryTime + " seconds. ";
+            var queryTime = data.responseHeader.QTime;
+    	   $scope.comment = "The query takes " + queryTime + " milliseconds. ";
+   });
+
+
       
   });
 });
