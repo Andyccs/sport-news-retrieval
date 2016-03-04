3e0174863a1af23f9b70e2a27f0cb0b74dd7d2a9
cyong1230 <cyong1230@yahoo.com>
2016-03-04 15:47:30 +0800

    Reformat code

--- crawler/recrawler/views.py
+++ crawler/recrawler/views.py
@@ -7,29 +7,33 @@ import json
 import urllib2
 import socket
 
+
 class MyException(Exception):
-	pass
+  pass
+
 
 def updateJSON():
-	# crawl json from twitter
-	result_json = news_crawler.crawl('espn', 1)
-	print result_json
+  # crawl json from twitter
+  result_json = news_crawler.crawl('espn', 1)
+  print result_json
 
-	# send json file to SOLR server
-	try:
-		req = urllib2.Request(url='http://localhost:8983/solr/update/json?commit=true', data=result_json)
-		req.add_header('Content-type', 'application/json')
-		response = urllib2.urlopen(req, timeout=2)
-	except urllib2.URLError, e:
-		print type(e)
-		raise MyException("Error: %r" % e)
-	except socket.timeout, e:
-		print type(e)
-		raise MyException("Error: %r" % e)
+  # send json file to SOLR server
+  try:
+    req = urllib2.Request(url='http://localhost:8983/solr/update/json?commit=true',
+                          data=result_json)
+    req.add_header('Content-type', 'application/json')
+    response = urllib2.urlopen(req, timeout=2)
+  except urllib2.URLError, e:
+    print type(e)
+    raise MyException("Error: %r" % e)
+  except socket.timeout, e:
+    print type(e)
+    raise MyException("Error: %r" % e)
+
 
 @csrf_exempt
 def recrawl(request):
-	print "BEGIN"
-	p = Pool(processes=1)
-	result = p.apply_async(updateJSON)
-	return HttpResponse("Recrawling")
+  print "BEGIN"
+  p = Pool(processes=1)
+  result = p.apply_async(updateJSON)
+  return HttpResponse("Recrawling")
