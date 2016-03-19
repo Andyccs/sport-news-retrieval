dd94252e8d5c0b615864f550f21e3f1f63b7f929
Ruan Pingcheng <ruanpingcheng@gmail.com>
2016-03-19 17:49:32 +0800

    try some code for auto-complete. But not working yet

--- UI/js/custom.js
+++ UI/js/custom.js
@@ -7,6 +7,62 @@ var keywords;
 
 
 app.controller('newsCtrl', function($scope, $http) {
+//For auto-complete feature
+// $scope.isDisabled    = false;
+// // list of `state` value/display objects
+// $scope.states        = loadAll();
+// $scope.querySearch   = querySearch;
+// $scope.selectedItemChange = selectedItemChange;
+// $scope.searchTextChange   = searchTextChange;
+//
+// function querySearch (query) {
+//   var results = query ? $scope.states.filter( createFilterFor(query) ) : $scope.states;
+//
+//   return results;
+// }
+//
+// function searchTextChange(text) {
+//   $log.info('Text changed to ' + text);
+// }
+//
+// function selectedItemChange(item) {
+//   $log.info('Item changed to ' + JSON.stringify(item));
+// }
+//
+//    /**
+//     * Build `states` list of key/value pairs
+//     */
+// function loadAll() {
+//      var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
+//              Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
+//              Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
+//              Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
+//              North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
+//              South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
+//              Wisconsin, Wyoming';
+//
+//   return allStates.split(/, +/g).map( function (state) {
+//     return {
+//       value: state.toLowerCase(),
+//       display: state
+//     };
+//   });
+// }
+//
+//    /**
+//     * Create filter function for a query string
+//     */
+// function createFilterFor(query) {
+//   var lowercaseQuery = angular.lowercase(query);
+//
+//   return function filterFn(state) {
+//     return (state.value.indexOf(lowercaseQuery) === 0);
+//   };
+//
+// }
+
+
+
 	
   //Init the date range from 2 years ago until now
   $scope.endDate = new Date();
